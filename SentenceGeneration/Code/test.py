a36e1c603a35974d96cf554129effa211787a4a2
Bidhan Bashyal <bidhanbashyal@Bidhans-Mac-mini.local>
2023-12-03 03:10:22 -0500

    aga

--- SentenceGeneration/Code/test.py
+++ SentenceGeneration/Code/test.py
@@ -5,9 +5,16 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 import re
+gpus_to_use = [0, 1, 2, 3]
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = "meta-llama/Llama-2-7b-chat-hf" # meta-llama/Llama-2-70b-hf
 tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
+
+if torch.cuda.is_available():
+    model = model.to(device)
+    model = torch.nn.DataParallel(model, device_ids=gpus_to_use)
+
 #prompt_path='/home/zhangxit/files/DataAug4SocialBias/SentenceGeneration/Data/prompts/prompt_gender.txt'
 #sentences_path = 'home/zhangxit/files/DataAug4SocialBias/SentenceGeneration/Data/longer_text10k_gender.txt'
 prompt_path='/scratch0/bashyalb/DataAug4SocialBias/SentenceGeneration/Data/prompts/prompt1.txt'
