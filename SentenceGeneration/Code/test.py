c48dc01b0a4e887eeb1d2ca73ed3aa7336d307ee
Bidhan Bashyal <bidhanbashyal@Bidhans-Mac-mini.local>
2023-12-08 11:53:17 -0500

    agfa

--- SentenceGeneration/Code/test.py
+++ SentenceGeneration/Code/test.py
@@ -37,7 +37,7 @@ def return_relevant_sentences(sentences_path, prompt_path):
     with open(sentences_path, 'r', encoding='utf-8') as file:
         sentences = [line.strip() for line in file]
         i=0
-    for sentence in sentences[0:25]:
+    for sentence in sentences[0:10]:
         if i%1==0:
           print('Done with',i,'sentences')
         i+=1
