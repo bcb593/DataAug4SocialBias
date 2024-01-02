ef5f090c9c29aa1115fc519084e0da240ac60fa9
bcb593 <52859294+bcb593@users.noreply.github.com>
2024-01-02 11:04:07 -0500

    afa

--- SentenceGeneration/Code/test.py
+++ SentenceGeneration/Code/test.py
@@ -40,7 +40,7 @@ def return_relevant_sentences(sentences_path, prompt_path):
     with open(sentences_path, 'r', encoding='utf-8') as file:
         sentences = [line.strip() for line in file]
 
-    for i, sentence in enumerate(tqdm(sentences[:100], desc="Processing sentences")):
+    for i, sentence in enumerate(tqdm(sentences, desc="Processing sentences")):
         print(sentence)
         full_prompt = f"{prompt}#Original Sentence#:{sentence}\"Output:"
         aug_sent = get_llama_response(full_prompt)
